e7389801640c56a29b5af31e16be04fb12b0f89e
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-11-21 02:28:40 +0300

    update header and footer to be responsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,22 @@
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 h-12 w-full border-t border-[#1E2D3D]">
-      <div className="grid h-full grid-cols-3 divide-x divide-[#1E2D3D] md:grid-cols-12">
-        <div className="font-fira col-span-2 inline-flex items-center pl-5 align-middle text-base text-slate-500 hover:text-[#43D9AD] md:pl-2 lg:pl-5">find me on:</div>
-        <div></div>
-        <div></div>
-        <div className="col-span-6 hidden md:flex"></div>
-        <div className="col-span-2"></div>
+    <div className="fixed bottom-0 left-0 hidden h-12 w-full border-t border-[#1E2D3D] text-slate-500 md:block">
+      <div className="grid h-full grid-cols-3 divide-x divide-[#1E2D3D] md:grid-cols-6 lg:grid-cols-12">
+        <div className="font-fira col-span-2 hidden items-center pl-5 align-middle text-base hover:text-[#43D9AD] md:pl-2 lg:inline-flex lg:pl-5">find me on:</div>
+        <a href="https://twitter.com/CedrouseR" target="_blank" className="flex items-center justify-center">
+          <i className="i-carbon-logo-twitter" style={{ fontSize: '20px' }}></i>
+        </a>
+        <a href="https://www.linkedin.com/in/cedrouseroll-omondi-44b119252/" target="_blank" className="flex items-center justify-center">
+          <i className="i-carbon-logo-linkedin" style={{ fontSize: '20px' }}></i>
+        </a>
+        <a href="https://github.com/zedjarvis" target="_blank" className="flex items-center justify-center">
+          <i className="i-carbon-logo-github" style={{ fontSize: '20px' }}></i>
+        </a>
+        <div className="col-span-4 hidden lg:flex"></div>
+        <div className="col-span-3 hidden items-center justify-end pr-5 text-xs md:flex lg:justify-center">
+          <p>&gt;_ made with ❤️ by yours truly</p>
+        </div>
       </div>
     </div>
   )
--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -4,7 +4,7 @@ import NavLink from './NavLink';
 export default function Header() {
   return (
     <div className="fixed left-0 top-0 h-12 w-full border-b border-[#1E2D3D]">
-      <div className="grid h-full grid-cols-3 divide-x divide-[#1E2D3D] md:grid-cols-6 lg:grid-cols-12">
+      <div className="grid h-full grid-cols-3 divide-[#1E2D3D] md:grid-cols-6 md:divide-x lg:grid-cols-12">
         <Logo />
         <NavLink url='/' text={'hello'} />
         <NavLink url='/about' text={'about-me'} />
@@ -12,6 +12,9 @@ export default function Header() {
         <NavLink url='/blog' text={'blog'} />
         <div className='col-span-5 hidden lg:flex'></div>
         <NavLink url='/contact' text={'contact'} />
+        <div className='flex items-center justify-end px-5 md:hidden'>
+          <i className='i-carbon-menu' style={{fontSize: '25px'}}></i>
+        </div>
       </div>
     </div>
   )
