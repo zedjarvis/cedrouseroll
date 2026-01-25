526f06bdfc4cb559a60d2a923f3dbf2ceca26c4c
zedjarvis <omondicedo@gmail.com>
2026-01-26 01:19:17 +0300

    feat: section title text

---
+++ app/components/SectionTitle.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { text } = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <p class="font-geist text-xs text-[#8f8f8f] opacity-60">{{ text.toUpperCase() }}</p>
+</template>
