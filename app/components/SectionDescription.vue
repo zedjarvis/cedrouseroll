e1a665a422bf006aef1e72c6b44685816b6c2d2a
zedjarvis <omondicedo@gmail.com>
2026-01-26 01:19:06 +0300

    feat: section description text

---
+++ app/components/SectionDescription.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { text } = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <p class="text-[#8f8f8f] tracking-wide leading-6">
+    {{ text }}
+  </p>
+</template>
