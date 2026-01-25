967f21a353479b596b36ba671d335472f87ddf7b
zedjarvis <omondicedo@gmail.com>
2026-01-26 01:18:51 +0300

    feat: add personal section component

---
+++ app/components/PersonalSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section
+    id="personal"
+    class="flex flex-col justify-center items-center w-full"
+  >
+    <div
+      class="w-full max-w-2xl mx-auto gap-8 flex flex-col items-start h-min px-4 relative scroll-mt-6"
+    >
+      <div
+        class="flex flex-col content-start items-start gap-4 w-full h-min relative"
+      >
+        <div class="flex-none whitespace-pre">
+          <SectionTitle text="Personal" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
