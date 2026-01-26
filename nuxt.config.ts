c3681520be70872e9dfea802644cf11c8f8fa36c
zedjarvis <omondicedo@gmail.com>
2026-01-26 19:08:08 +0300

    chore: add nuxt-supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  ssr: true,
+  ssr: false,
 
   css: ["~/assets/css/tailwind.css"],
   modules: [
@@ -19,7 +19,13 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "shadcn-nuxt",
     "@nuxtjs/color-mode",
+    "@nuxtjs/supabase",
   ],
+  supabase: {
+    url: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NUXT_PUBLIC_SUPABASE_KEY,
+    redirect: false,
+  },
   shadcn: {
     prefix: "",
     componentDir: "@/components/ui",
@@ -36,4 +42,33 @@ export default defineNuxtConfig({
       { name: "Geist Mono", provider: "google" },
     ],
   },
+  app: {
+    head: {
+      meta: [{ name: "theme-color", content: "#0a0a0a" }],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+      ],
+    },
+  },
 });
+
