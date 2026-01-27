a8d69b372ab3c57f46e0f376b062c2f129fe211d
zedjarvis <omondicedo@gmail.com>
2026-01-27 03:34:21 +0300

    refactor: only display available blog

--- app/components/WritingSection.vue
+++ app/components/WritingSection.vue
@@ -17,59 +17,60 @@ import { Clock } from "lucide-vue-next";
     </div>
 
     <div class="flex flex-col w-full">
-      <div
-        class="hover:bg-[#141414] transition-all ease-out duration-200 flex justify-center py-2"
-      >
-        <div
-          class="w-full mx-auto max-w-2xl px-4 flex items-center justify-between"
-        >
-          <div
-            class="flex flex-col md:grid md:grid-cols-12 w-full items-start md:items-center"
-          >
-            <div class="col-span-2">
-              <p class="text-sm text-muted-foreground font-geist tabular-nums">
-                26/01/26
-              </p>
-            </div>
-            <div class="col-span-8">
-              <p>How to think like both a designer & engineer</p>
-            </div>
-            <div
-              class="col-span-2 flex gap-2 items-center justify-end text-sm text-muted-foreground"
-            >
-              <Clock :size="15" /> <span>2</span> <span>m</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- <div -->
+      <!--   class="hover:bg-[#141414] transition-all ease-out duration-200 flex justify-center py-2" -->
+      <!-- > -->
+      <!--   <div -->
+      <!--     class="w-full mx-auto max-w-2xl px-4 flex items-center justify-between" -->
+      <!--   > -->
+      <!--     <div -->
+      <!--       class="flex flex-col md:grid md:grid-cols-12 w-full items-start md:items-center" -->
+      <!--     > -->
+      <!--       <div class="col-span-2"> -->
+      <!--         <p class="text-sm text-muted-foreground font-geist tabular-nums"> -->
+      <!--           26/01/26 -->
+      <!--         </p> -->
+      <!--       </div> -->
+      <!--       <div class="col-span-8"> -->
+      <!--         <p>How to think like both a designer & engineer</p> -->
+      <!--       </div> -->
+      <!--       <div -->
+      <!--         class="col-span-2 flex gap-2 items-center justify-end text-sm text-muted-foreground" -->
+      <!--       > -->
+      <!--         <Clock :size="15" /> <span>2</span> <span>m</span> -->
+      <!--       </div> -->
+      <!--     </div> -->
+      <!--   </div> -->
+      <!-- </div> -->
 
-      <div
-        class="hover:bg-[#141414] transition-all ease-out duration-200 flex justify-center py-2"
-      >
-        <div
-          class="w-full mx-auto max-w-2xl px-4 flex items-center justify-between"
-        >
-          <div
-            class="flex flex-col md:grid md:grid-cols-12 w-full items-start md:items-center"
-          >
-            <div class="col-span-2">
-              <p class="text-sm text-muted-foreground font-geist tabular-nums">
-                26/01/26
-              </p>
-            </div>
-            <div class="col-span-8">
-              <p>How AI is changing my workflow</p>
-            </div>
-            <div
-              class="col-span-2 flex gap-2 items-center justify-end text-sm text-muted-foreground"
-            >
-              <Clock :size="15" /> <span>2</span> <span>m</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- <div -->
+      <!--   class="hover:bg-[#141414] transition-all ease-out duration-200 flex justify-center py-2" -->
+      <!-- > -->
+      <!--   <div -->
+      <!--     class="w-full mx-auto max-w-2xl px-4 flex items-center justify-between" -->
+      <!--   > -->
+      <!--     <div -->
+      <!--       class="flex flex-col md:grid md:grid-cols-12 w-full items-start md:items-center" -->
+      <!--     > -->
+      <!--       <div class="col-span-2"> -->
+      <!--         <p class="text-sm text-muted-foreground font-geist tabular-nums"> -->
+      <!--           26/01/26 -->
+      <!--         </p> -->
+      <!--       </div> -->
+      <!--       <div class="col-span-8"> -->
+      <!--         <p>How AI is changing my workflow</p> -->
+      <!--       </div> -->
+      <!--       <div -->
+      <!--         class="col-span-2 flex gap-2 items-center justify-end text-sm text-muted-foreground" -->
+      <!--       > -->
+      <!--         <Clock :size="15" /> <span>2</span> <span>m</span> -->
+      <!--       </div> -->
+      <!--     </div> -->
+      <!--   </div> -->
+      <!-- </div> -->
 
-      <div
+      <NuxtLink
+        to="/writing/hello-world"
         class="hover:bg-[#141414] transition-all ease-out duration-200 flex justify-center py-2"
       >
         <div
@@ -93,7 +94,7 @@ import { Clock } from "lucide-vue-next";
             </div>
           </div>
         </div>
-      </div>
+      </NuxtLink>
     </div>
   </section>
 </template>
