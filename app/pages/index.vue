71bbd362b1f8cfd77ff5f04025b8c2f6c7fbf63d
zedjarvis <omondicedo@gmail.com>
2026-02-01 03:21:58 +0300

    style: add `z-index`

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -75,7 +75,7 @@ onMounted(() => {
     </main>
     <div
       v-if="showBlur"
-      class="fixed bottom-0 left-0 w-full pointer-events-none"
+      class="fixed bottom-0 left-0 w-full pointer-events-none z-12"
     >
       <GradualBlur
         target="parent"
